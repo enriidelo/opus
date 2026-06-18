a0d783ba03f57b47b958818a9f08fe0935c85839
enriidelo <enrydelorenzi09@gmail.com>
2026-06-18 10:14:17 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+// Cambia questo numero ad ogni deploy per invalidare la cache
+const VERSION = '1.0.0';
+const CACHE = 'fh-v' + VERSION;
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    // Network-first: tenta sempre la rete, fallback su cache
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
